130b12f5e5bb81ba7780ac398ca04b3074f1b763
nil-natal <nil-natal@hotmail.com>
2024-07-13 00:26:26 -0300

    teste1

--- monitoria.php
+++ monitoria.php
@@ -10,7 +10,7 @@ include "conexao.php";
     <link rel="stylesheet" type="text/css" href="style_print.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Monitor Impressoras</title>
+    <title>SICCI - CLBI</title>
 </head>
 
 <body>
